b4c6576689283fd2695b33fca98e75ef9c41b6a7
Jakob Olsen <jakob@bikeolsen.dk>
2026-06-04 15:29:07 +0200

    get specific session

--- f1_analysis/__main__.py
+++ f1_analysis/__main__.py
@@ -2,31 +2,49 @@ from f1_analysis.data_manipulation.lap_data import (
     get_car_data_per_lap,
     get_fastest_laptime,
 )
-from f1_analysis.data_structures.df_columns import CarDataDF
+from f1_analysis.data_manipulation.session_data import get_session_key
+from f1_analysis.data_structures.df_columns import CarDF, SessionDataColumns
 from f1_analysis.openf1_api import OpenF1API
 
 
-def main(year: int) -> None:
+def main(
+    year: int,
+    session_name: str = "Qualifying",
+    session_attribute: str = SessionDataColumns.CIRCUIT_SHORT_NAME,
+    session_value: str = "Spa-Francorchamps",
+) -> None:
     """Run main module of ``f1_analysis``.
 
     Parameters
     ----------
     year : int
         Year to analyze
+    session_name : str
+        The type of session (more specific than session_type). See
+        [api](https://openf1.org/docs/#sessions) for documentation.
+    session_attribute : str
+        Attribute from the sessions endpoint to match session_value against. Default is
+        SessionDataColumns.CIRCUIT_SHORT_NAME
+    session_value : str
+        Value to match a column against. Default "Spa-Francorchamps"
 
     Returns
     -------
     None
     """
     f1_api = OpenF1API()
-    session_keys = f1_api.get_session_keys(year)
+    session_df = f1_api.get_session_data(year)
 
     # for now choose first key but switch to get_session_key when implemented
-    session_key = session_keys[0]
-    # session_key = get_session_key()
+    session_key = get_session_key(
+        session_df,
+        session_name=session_name,
+        key_name=session_attribute,
+        matching_value=session_value,
+    )
 
     drivers = f1_api.get_session_drivers(session_key)
-    car_dat_per_driver: dict[int, CarDataDF] = {}
+    car_dat_per_driver: dict[int, CarDF] = {}
     for driver in drivers:
         car_data = f1_api.get_session_car_data(session_key, driver.number)
         laps = f1_api.get_lap_data(session_key, driver.number)
--- f1_analysis/data_manipulation/session_data.py
+++ f1_analysis/data_manipulation/session_data.py
@@ -1,10 +1,18 @@
 import polars as pl
 
+from f1_analysis.data_structures.df_columns import SessionDataColumns, SessionDF
+
 
 def get_session_key(
-    session_df: pl.DataFrame, location: str | None = None, country: str | None = None
+    session_df: SessionDF,
+    session_name: str = "Qualifying",
+    key_name: str = SessionDataColumns.CIRCUIT_SHORT_NAME,
+    matching_value: str = "Spa-Francorchamps",
 ) -> int:
     """Get a list of sessions and filter by keywords to extract just one session.
+
+    ``key_name`` is a column of ``SessionDataColumns`` and ``matching_value`` is the
+    value that will be looked for in that column.
 
     If there are multiple sessions with the provided filters, then the first one is
     chosen.
@@ -13,15 +21,31 @@ def get_session_key(
     ----------
     session_df : pl.DataFrame
         Sessions to select from
-    location : str | None, optional
-        Location like "Spa-Francorchamps". This will not be strictly compared, so 'spa'
-        is sufficient to get this event. by default None
-    country : str | None, optional
-        Gets the event(s) in the given country, by default None
+    session_nane : str
+        Strictly matching of session name. Session type does not distinguish
+        "Qualifying" and "Sprint Qualifying", but session_name does.
+        Default "Qualifying"
+    key_name : str, optional
+        Key to look for in dataframe. Default SessionDataColumns.CIRCUIT_SHORT_NAME
+    matching_value : str, optional
+        Value to match against in the df of column key_name. Default "Spa-Francorchamps"
 
     Returns
     -------
     int
         session key given the filter arguments.
     """
-    raise NotImplementedError("")
+    df = SessionDataColumns.validate(session_df)
+
+    lower_case_col = pl.col(key_name).str.to_lowercase()
+    lower_case_val = matching_value.lower()
+
+    df_filtered = df.filter(lower_case_col.str.contains(lower_case_val)).filter(
+        pl.col(SessionDataColumns.SESSION_NAME) == session_name
+    )
+    session_key = df_filtered[SessionDataColumns.SESSION_KEY].first()
+
+    if not isinstance(session_key, int):
+        raise ValueError("Unable to extract session_key from DataFrame")
+
+    return session_key
